f5a929712663151d22c553850999021be5dd0995
Newton <narmstrongv@gmail.com>
2021-09-14 12:36:28 -0700

    setting up the store/reducer

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ The Shop-Shop-Shop store is a refactored e-commerce site from the module excerci
 # Screenshot
 
 # Link
+
+# Note
+1. Set up Store
+2. Set up Reducer
+3. Set up Action
+4. Implement Dispatch 
+5. Load 
+
+How to set up the store
+What exactly is the store?
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
+import { Provider } from 'react-redux'
+import { reducer, useProductReducer } from './utils/reducers'
+import { render } from 'react-dom'
+
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(reducer, composeWithDevTools(
+  //applyMiddleware(...middleware),
+  // other store enhancers if any
+));
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
+)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
